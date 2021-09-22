91277b927b6d32be04374b3cc1897a2bb1f1db2e
AbdulRehmanKhursheed <59060012+AbdulRehmanKhursheed@users.noreply.github.com>
2021-09-22 05:39:23 -0700

    Update getusers.js

--- functions/getusers.js
+++ functions/getusers.js
@@ -11,40 +11,40 @@
 
 
 // // };
-// const axios = require("axios");
+ const axios = require("axios");
 
-// exports.handler = async function (event, context, callback) {
-//   const data = await axios("https://jsonplaceholder.typicode.com/photos");
-//   if (event.httpMethod == "GET") {
+ exports.handler = async function (event, context, callback) {
+   const data = await axios("https://jsonplaceholder.typicode.com/photos");
+   if (event.httpMethod == "GET") {
 
-//     callback(null, {
-//       statusCode: 200,
-//       body: JSON.stringify({
-//         data:data.data ,
-//       }),
-//     });
-//   } else if (event.httpMethod == "POST") {
-//     callback(null, {
-//       statusCode: 200,
-//       body: JSON.stringify({
-//         msg: "Post request recieved !",
-//       }),
-//     });
-//   } else {
-//     callback(null, {
-//       statusCode: 200,
-//       body: "Not a `get` request neither `post` ",
-//     });
-//   }
-// };
-exports.handler = async (event, context) => {
-  setTimeout(function(){
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Serverless Functions are cool! 😎"
-      })
-    }
-  },11000);
+     callback(null, {
+       statusCode: 200,
+       body: JSON.stringify({
+         data:data.data ,
+       }),
+     });
+   } else if (event.httpMethod == "POST") {
+     callback(null, {
+       statusCode: 200,
+       body: JSON.stringify({
+         msg: "Post request recieved !",
+       }),
+     });
+   } else {
+     callback(null, {
+       statusCode: 200,
+      body: "Not a `get` request neither `post` ",
+     });
+   }
+ };
+//exports.handler = async (event, context) => {
+ // setTimeout(function(){
+ //   return {
+   //   statusCode: 200,
+  //    body: JSON.stringify({
+   //     message: "Serverless Functions are cool! 😎"
+   //   })
+   // }
+  //,11000);
   
-}
+//}
